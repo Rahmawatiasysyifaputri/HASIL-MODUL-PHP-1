7c994e751a04ef72a9b0c94e69a0b1d8d45abc70
Rahmawati Asy syifa' Putri 27 RPL <42015213+Rahmawatiasysyifaputri@users.noreply.github.com>
2019-02-20 14:53:44 +0700

    Add files via upload

---
+++ Latihan 4.php
@@ -0,0 +1,8 @@
+<?php
+  $nama = "Puput";
+  function tampil_nama() {
+      global $nama;
+      echo "Nama Saya :".$nama;
+    }
+    tampil_nama();
+?>
---
+++ contoh.php
@@ -0,0 +1,16 @@
+<?php
+  $nama = "Puput";
+  $sekolah = "SMK Telkom Malang";
+  $umur = 17;
+
+  //menampilkan nilai variabel
+  //contoh
+  echo "Halo namaku $nama usiaku $umur tahun";
+  echo "<br>";
+  echo "Aku sekolah di $sekolah";
+
+  //contoh2
+  echo 'Halo namaku'.$nama.'usiaku'.$umur.'tahun';
+  echo "<br>";
+  echo 'Aku sekolah di'.$sekolah;
+  ?>
